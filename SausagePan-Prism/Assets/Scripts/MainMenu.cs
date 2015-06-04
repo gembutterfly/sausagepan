67a03c306b269d854cffeb36d96506a2c8ef4ab2
Stephanie Sara Groß <s7898910@mail.zih.tu-dresden.de>
2015-06-04 22:22:29 +0200

    added MainMenu

---
+++ SausagePan-Prism/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	public int level = 0;
+	
+	public void loadNewLevel() {
+		Application.LoadLevel (level);
+	}
+
+	public void endGame() {
+		Application.Quit();
+	}
+
+	public void Update() {
+		if (AudioSource.mute) {
+
+		}
+
+	}
+}
