8591c50161e7f0085887e10d287a4df132245100
Stephanie Sara Groß <s7898910@mail.zih.tu-dresden.de>
2015-06-05 22:10:21 +0200

    startscreen fertig

--- SausagePan-Prism/Assets/Scripts/MainMenu.cs
+++ SausagePan-Prism/Assets/Scripts/MainMenu.cs
@@ -7,8 +7,20 @@ public class MainMenu : MonoBehaviour {
 	public int level = 0;
 	public Image sound;
 
+	public bool IsStartscreen = false;
+	public float waitingTime = 1.0f;
+
+	//spam für fade-in/fade-out zu Szenen
+	public Texture2D fadeOutTexture;
+	public float fadeSpeed = 0.8f;
+
+	private int drawDepth = -1000;
+	private float alpha = 1.0f;
+	private int fadeDir = -1;
+	//spam ende
+
 	public void loadNewLevel() {
-		Application.LoadLevel (level);
+		StartCoroutine("ChangeLevel");
 	}
 
 	public void endGame() {
@@ -26,5 +38,37 @@ public class MainMenu : MonoBehaviour {
 	{
 		AudioListener.volume = 1;
 		sound.enabled = true;
+	}
+
+	//weiterer spam für fade-in/fade-out
+	void OnGUI () {
+	
+		alpha += fadeDir * fadeSpeed * Time.deltaTime;
+		alpha = Mathf.Clamp01 (alpha); //wert zw. 0 und 1
+
+		GUI.color = new Color (GUI.color.r, GUI.color.g, GUI.color.b, alpha);
+		GUI.depth = drawDepth;
+		GUI.DrawTexture (new Rect (0, 0, Screen.width, Screen.height), fadeOutTexture);
+	}
+
+	//setzt fadeDir auf direction: -1 = fade-in; 1 = fade-out
+	public float BeginFade (int direction) {
+		fadeDir = direction;
+		return (fadeSpeed);
+	}
+
+	void Start () {
+		BeginFade (-1);
+	}
+
+	IEnumerator ChangeLevel () {
+		float fadeTime = BeginFade (1);
+		yield return new WaitForSeconds (fadeTime);
+		Application.LoadLevel (level);
+	}
+
+	void Update () {
+		if (IsStartscreen)
+			Invoke ("loadNewLevel", waitingTime);
 	}
 }
