1023888863906ba88fd27f7d9b0b616476b26af1
Stephanie Sara Groß <s7898910@mail.zih.tu-dresden.de>
2015-06-17 21:56:39 +0200

    Lvl 3 Anfänge und Hilfetext

---
+++ SausagePan-Prism/Assets/Scripts/Cloud_Absorbtion.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cloud_Absorbtion : MonoBehaviour {
+
+	private Animator anim;
+
+
+
+
+	void OnCollisionEnter2D(Collision2D coll) {
+		if (coll.gameObject.tag == "GodRay") {
+			anim = coll.gameObject.GetComponent<Animator> ();
+			anim.SetBool ("IsBlocked", true);
+			Debug.Log("I know it hit me");
+		}
+	}
+
+	void OnCollisionExit2D(Collision2D coll) {
+		if (coll.gameObject.tag == "GodRay") {
+			anim = coll.gameObject.GetComponent<Animator> ();
+			anim.SetBool ("IsBlocked", false);
+			Debug.Log("I know it exited me");
+		}
+	}
+
+
+
+
+}
---
+++ SausagePan-Prism/Assets/Scripts/Windmill.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Windmill : MonoBehaviour {
+	
+	private Animator animCloud;
+	private Animator animMill;
+
+	void Start () {
+		animCloud = GameObject.Find("CloudSet_1").GetComponent<Animator> ();
+		animMill = GetComponent<Animator> ();
+	}
+
+
+	void OnMouseDown(){
+		animCloud.SetBool ("IsMoved", true);
+		animMill.SetBool ("IsClicked", true);
+
+	} 
+}
