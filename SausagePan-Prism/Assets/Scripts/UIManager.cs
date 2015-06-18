5e61e7f7b1a98ca8bdaf6568f4d6c3480b4b5e37
sergejSDA <sergej.s.d.a@googlemail.com>
2015-06-18 11:45:40 +0200

    clouds

--- SausagePan-Prism/Assets/Scripts/UIManager.cs
+++ SausagePan-Prism/Assets/Scripts/UIManager.cs
@@ -4,11 +4,19 @@ using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour {
 
+	public GameObject blackCloud;
+	public GameObject whiteCloud;
+
 	GameObject sound;
+	GameObject player;
+	bool blackCloudIsActive = false;
+	bool whiteCloudIsActive = false;
 
 	public void Start() {
 		sound = GameObject.Find ("Sound");
 		this.gameObject.AddComponent<GlobalSound>();
+
+		player = GameObject.FindGameObjectWithTag("Player");
 	}
 
 	public void Mute()
@@ -39,5 +47,45 @@ public class UIManager : MonoBehaviour {
 
 	public void RestartLevel() {
 		Application.LoadLevel (Application.loadedLevel);
+	}
+
+	public void CallBlackCloud()
+	{
+		if (!blackCloudIsActive) 
+		{
+			blackCloudIsActive = true;
+
+			blackCloud.SetActive (true);
+			blackCloud.GetComponent<Transform> ().localPosition = new Vector3 (player.GetComponent<Transform> ().localPosition.x,
+		                                                                 	   player.GetComponent<Transform> ().localPosition.y + 5,
+		                                                                  	   player.GetComponent<Transform> ().localPosition.z);
+			Invoke ("ResetBlackCloud", 5);
+		}
+	}
+
+	public void CallWhiteCloud()
+	{
+		if (!whiteCloudIsActive) 
+		{
+			whiteCloudIsActive = true;
+
+			whiteCloud.SetActive (true);
+			whiteCloud.GetComponent<Transform> ().localPosition = new Vector3 (player.GetComponent<Transform> ().localPosition.x,
+		                                                                   	   player.GetComponent<Transform> ().localPosition.y + 5,
+		                                                                   	   player.GetComponent<Transform> ().localPosition.z);
+			Invoke ("ResetWhiteCloud", 5);
+		}
+	}
+
+	void ResetBlackCloud()
+	{
+		blackCloud.SetActive (false);
+		blackCloudIsActive = false;
+	}
+
+	void ResetWhiteCloud()
+	{
+		whiteCloud.SetActive (false);
+		whiteCloudIsActive = false;
 	}
 }
