760253c92f4f3d4f37c1b464eb8a6b7728138afe
Alexandra Krien <alexandra.krien@gmail.com>
2015-07-02 13:53:19 +0200

    extend zwischenseq1

---
+++ SausagePan-Prism/Assets/Scripts/Zwischensequenz/switchExpl.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class switchExpl : MonoBehaviour {
+	public Sprite expl2;
+	public Sprite expl3;
+	public Sprite expl4;
+	public Sprite expl5;
+	public Sprite expl6;
+	public Sprite expl7;
+	public Sprite expl8;
+	public Sprite expl9;
+	public Sprite expl10;
+
+	private Sprite[] order;
+
+	private int wait = 100;
+	private int next = 0;
+	// Use this for initialization
+	void Start () {
+		order = new Sprite[9]{expl2, expl3, expl4, expl5, expl6, expl7, expl8, expl9, expl10};
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (wait == 0 && next < 9) {
+			SpriteRenderer help = GetComponent<SpriteRenderer> ();
+
+			help.sprite = order[next];
+			wait = 100;
+			next++;
+		}
+		wait--;
+	}
+}
---
+++ SausagePan-Prism/Assets/Scripts/Zwischensequenz/zwischenseq1.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class zwischenseq1 : MonoBehaviour {
+	public int scenecount;
+	private int zahl;
+	private int[] time = new int[]{180, 150, 1000, 300};
+
+	public GameObject explanation;
+	public Sprite expl2;
+	public Sprite expl3;
+	public Sprite expl4;
+	public Sprite expl5;
+	public Sprite expl6;
+	public Sprite expl7;
+	public Sprite expl8;
+	public Sprite expl9;
+	public Sprite expl10;
+	
+	private Sprite[] order;
+	
+	private int wait = 100;
+	private int next = 0;
+
+	public GameObject lastScreen;
+	public Sprite last;
+
+	// Use this for initialization
+	void Start () {
+		zahl = time [scenecount - 1];
+		order = new Sprite[9]{expl2, expl3, expl4, expl5, expl6, expl7, expl8, expl9, expl10};
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (scenecount == 3) {
+			if (wait == 0 && next < 9) {
+				SpriteRenderer help = explanation.GetComponent<SpriteRenderer> ();
+				
+				help.sprite = order[next];
+				wait = 100;
+				next++;
+			}
+			wait--;
+		}
+
+		if (scenecount == 4 && zahl == 150) {
+			SpriteRenderer help = lastScreen.GetComponent<SpriteRenderer> ();
+			
+			help.sprite = last;
+		}
+		if (zahl == 0)
+			nextScene ();
+		else
+			zahl--;
+	}
+
+	private void nextScene(){
+		scenecount++;
+		if (scenecount > 4)
+			Application.LoadLevel ("Level2");
+		else
+			Application.LoadLevel ("szene" + scenecount);
+	}
+
+	public void Skip(){
+		/*var go = GameObject.Find ("audio");
+		AudioSource help = go.GetComponent<AudioSource> ();
+		help.Stop ();*/
+		Application.LoadLevel ("Level2");
+	}
+}
