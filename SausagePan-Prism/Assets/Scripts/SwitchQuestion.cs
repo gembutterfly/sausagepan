7994237d49ddde8c450285d8254a9ee08b003c1b
Georg Eckert <eckert.georg@gmx.de>
2015-07-03 14:43:50 +0200

    fix quiz system

--- SausagePan-Prism/Assets/Scripts/SwitchQuestion.cs
+++ SausagePan-Prism/Assets/Scripts/SwitchQuestion.cs
@@ -4,30 +4,46 @@ using UnityEngine.UI;
 
 public class SwitchQuestion : MonoBehaviour {
 
-	public string question;
-	public string answer1;
-	public string answer2;
-	public string answer3;
-	public string answer4;
-	public int correctAnswer;
+	public QuizQuestion[] questions;
 	public GameObject buttonA;
 	public GameObject buttonB;
 	public GameObject buttonC;
 	public GameObject buttonD;
 	public GameObject questionText;
 	public GameObject feedback;
+	public int activeNumber = 0;
+
+	public void Start() {
+		questions = new QuizQuestion[10];
+		questions[0] = new QuizQuestion(
+			"Welche Farbe hat ein rein rotes Blatt Papier unter reinem grünem Licht?",
+			"Grün",
+			"Schwarz",
+			"Rot",
+			"Weiß",
+			2);
+		questions[1] = new QuizQuestion(
+			"Welche Lichtfarben muss man mischen um gelbes Licht zu erhalten?",
+			"Grün & Blau",
+			"Grün & Rot",
+			"Blau & Rot",
+			"Rot & Cyan",
+			2);
+	}
 
 
-	public void setAnswersAndQuestion() {
-		buttonA.GetComponentInChildren<Text>().text = answer1;
-		buttonB.GetComponentInChildren<Text>().text = answer2;
-		buttonC.GetComponentInChildren<Text>().text = answer3;
-		buttonD.GetComponentInChildren<Text>().text = answer4;
-		questionText.GetComponent<Text> ().text = question;
+
+	public void setAnswersAndQuestion(int question) {
+		activeNumber = question;
+		buttonA.GetComponentInChildren<Text> ().text = questions [question].answer1;
+		buttonB.GetComponentInChildren<Text>().text = questions [question].answer2;
+		buttonC.GetComponentInChildren<Text>().text = questions [question].answer3;
+		buttonD.GetComponentInChildren<Text>().text = questions [question].answer4;
+		questionText.GetComponent<Text> ().text = questions [question].question;
 	}
 
 	public void isAnswerCorrect(int answer) {
-		if (answer == correctAnswer)
+		if (answer == questions[activeNumber].correctAnswer)
 			feedback.GetComponent<Text>().text = "Richtig!";
 		else
 			feedback.GetComponent<Text>().text = "Falsch!";
