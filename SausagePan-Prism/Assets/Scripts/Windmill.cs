7fa085e452fef7f6394ed9ec959549b30ae8d1da
Stephanie Sara Groß <s7898910@mail.zih.tu-dresden.de>
2015-07-05 11:21:09 +0200

    Update Lvl3

--- SausagePan-Prism/Assets/Scripts/Windmill.cs
+++ SausagePan-Prism/Assets/Scripts/Windmill.cs
@@ -5,16 +5,50 @@ public class Windmill : MonoBehaviour {
 	
 	private Animator animCloud;
 	private Animator animMill;
+	private bool isClicked = false;
+	private int cloudSet;
 
 	void Start () {
-		animCloud = GameObject.Find("CloudSet_1").GetComponent<Animator> ();
+		if (name == "windmill_stand_1") {
+			animCloud = GameObject.Find ("CloudSet_1").GetComponent<Animator> ();
+			cloudSet = 1;
+		}
+		if (name == "windmill_stand_2") {
+			animCloud = GameObject.Find ("CloudSet_2").GetComponent<Animator> ();
+			cloudSet = 2;
+		}
+		if (name == "windmill_stand_3") {
+			animCloud = GameObject.Find ("CloudSet_2").GetComponent<Animator> ();
+			cloudSet = 3;
+		}
+		if (name == "windmill_stand_4") {
+			animCloud = GameObject.Find ("CloudSet_2").GetComponent<Animator> ();
+			cloudSet = 4;
+		}
+
+
 		animMill = GetComponent<Animator> ();
 	}
 
+	void OnMouseEnter() {
+		gameObject.GetComponent<Renderer>().material.color = new Color(0f, 0f, 0f);
+	}
+	
+	void OnMouseExit() {
+		gameObject.GetComponent<Renderer>().material.color = new Color(1f, 1f, 1f);
+	}
 
 	void OnMouseDown(){
-		animCloud.SetBool ("IsMoved", true);
-		animMill.SetBool ("IsClicked", true);
+
+		if (isClicked == true) {
+			isClicked = false;
+			animCloud.SetInteger ("CloudSet", cloudSet * (-1));
+		} else {
+			isClicked = true;
+		animCloud.SetInteger ("CloudSet", cloudSet);
+		}
+
+		animMill.SetBool ("IsClicked", isClicked);
 
 	} 
 }
