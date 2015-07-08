9c84e0da109e42a1b0c344add0b2726be36fd14a
Alexandra Krien <alexandra.krien@gmail.com>
2015-07-08 12:31:29 +0200

    lvl2 menu fixed

---
+++ SausagePan-Prism/Assets/Scripts/Level 2/HelpScript_Lvl2.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HelpScript_Lvl2 : MonoBehaviour {
+
+	public GameObject minigame;
+	public GameObject helpText;
+	public GameObject helpMenu;
+	public GameObject backToTextBTN;
+	
+	/**
+	 * Show minigame in help
+	 * */
+	public void ShowMinigame()
+	{
+		minigame.SetActive (true);
+		helpText.SetActive (false);
+		backToTextBTN.SetActive (true);
+		
+
+	}
+	
+	/**
+	 * Exit button of help menu
+	 * */
+	public void ExitHelpMenue()
+	{
+		helpMenu.SetActive (false);
+		helpText.SetActive (true);
+		minigame.SetActive (false);
+		backToTextBTN.SetActive (false);
+	}
+	
+	/**
+	 * Set the minigame GameObject false and helpText one true
+	 * */
+	public void BackToHelpText()
+	{
+		helpText.SetActive (true);
+		minigame.SetActive (false);
+		backToTextBTN.SetActive (false);
+	}
+}
