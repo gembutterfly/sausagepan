b0f1597ecf62441fb7664987cade09cb0a3ae6eb
Bettina Nathalie Blasberg <bettina.blasberg@yahoo.de>
2015-07-09 17:38:57 +0200

    Glühwürmchen mit farbigen Po in Level 5 eingefügt

--- SausagePan-Prism/Assets/Scripts/Level 5/Switches/LightSwitchController.cs
+++ SausagePan-Prism/Assets/Scripts/Level 5/Switches/LightSwitchController.cs
@@ -8,6 +8,8 @@ public class LightSwitchController : MonoBehaviour {
 	public GameObject firstRay;
 	public GameObject secondRay;
 
+	public SpriteRenderer firefly;
+
 	private enum State
 	{
 		First,
@@ -16,6 +18,8 @@ public class LightSwitchController : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		lightState = State.Second;
+		paintFirefly (secondRay.GetComponent<GodRay>().rayColor);
+
 
 		secondRay.SetActive (true);
 		firstRay.SetActive (false);
@@ -33,10 +37,19 @@ public class LightSwitchController : MonoBehaviour {
 		{
 			switch(lightState)
 			{
-			case State.First: secondRay.SetActive(false); firstRay.SetActive(true); lightState = State.Second; break;
-			case State.Second: firstRay.SetActive(false); secondRay.SetActive(true); lightState = State.First; break;
+			case State.First: secondRay.SetActive(false); firstRay.SetActive(true); lightState = State.Second; paintFirefly (firstRay.GetComponent<GodRay>().rayColor); break;
+			case State.Second: firstRay.SetActive(false); secondRay.SetActive(true); lightState = State.First; paintFirefly (secondRay.GetComponent<GodRay>().rayColor); break;
 			}
 		}
 
+	}
+
+	void paintFirefly(string colorName)
+	{
+		switch (colorName) {
+		case "cyan": firefly.color = Color.cyan; break;
+		case "magenta": firefly.color = Color.magenta; break;
+		case "yellow": firefly.color = Color.yellow; break;
+		}
 	}
 }
