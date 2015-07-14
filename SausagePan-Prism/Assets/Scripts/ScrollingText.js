d11c4aa2ce8e297f0603f297288b1726e5889d30
Georg Eckert <eckert.georg@gmx.de>
2015-07-14 21:47:33 +0200

    add credits screen, make camera window bigger

---
+++ SausagePan-Prism/Assets/Scripts/ScrollingText.js
@@ -0,0 +1,19 @@
+﻿#pragma strict
+
+var speed = 10;
+var crawling = false;
+
+function Start () {
+	crawling = true;
+}
+
+function Update ()
+{
+    if (!crawling)
+        return;
+    transform.Translate(Vector3.up * Time.deltaTime * speed);
+    if (gameObject.transform.position.y > 2000)
+    {
+        gameObject.transform.position.y = 0;
+    }
+}
