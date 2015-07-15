f30d2f155be58b5d9b18860025d695ac3b341d17
sergejSDA <sergej.s.d.a@googlemail.com>
2015-07-15 18:19:52 +0200

    ein paar anpassungen

---
+++ SausagePan-Prism/Assets/Scripts/Level 4/CollisionScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollisionScript : MonoBehaviour {
+
+	private MothScript mothScript;
+
+	public void OnTriggerEnter2D (Collider2D other)
+	{
+		mothScript.IsTriggered ();	
+	}
+
+	// Use this for initialization
+	void Start () 
+	{
+		mothScript = GameObject.Find ("HelpManager").GetComponent<MothScript> ();
+	}
+}
