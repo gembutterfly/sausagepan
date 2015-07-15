86671a0ff82cc454818d309ea18901be970d006d
Alexandra Krien <alexandra.krien@gmail.com>
2015-07-15 22:36:29 +0200

    extend outro
    jetzt fehlt nur noch der sound

--- SausagePan-Prism/Assets/Scripts/Zwischensequenz/zwischenseq1.cs
+++ SausagePan-Prism/Assets/Scripts/Zwischensequenz/zwischenseq1.cs
@@ -19,7 +19,7 @@ public class zwischenseq1 : MonoBehaviour {
 	
 	private Sprite[] order;
 	
-	private int wait = 100;
+	private int wait = 200;
 	private int next = 0;
 
 	public GameObject lastScreen;
@@ -38,7 +38,7 @@ public class zwischenseq1 : MonoBehaviour {
 				SpriteRenderer help = explanation.GetComponent<SpriteRenderer> ();
 				
 				help.sprite = order[next];
-				wait = 100;
+				wait = 200;
 				next++;
 			}
 			wait--;
