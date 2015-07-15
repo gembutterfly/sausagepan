42f2152884fd26aa5120b65e5756b6e6dd00f2be
Alexandra Krien <alexandra.krien@gmail.com>
2015-07-15 11:02:02 +0200

    fix gate lvl2

--- SausagePan-Prism/Assets/Scripts/Gate.cs
+++ SausagePan-Prism/Assets/Scripts/Gate.cs
@@ -21,6 +21,8 @@ public class Gate : MonoBehaviour {
 	{
 		if (other.CompareTag ("Player")) 
 		{
+			Debug.Log(playerBody.color);
+			Debug.Log(color);
 			if(playerBody.color.Equals(color))
 			{
 				gateinner.SetActive(true);
@@ -52,7 +54,7 @@ public class Gate : MonoBehaviour {
 				color = Color.red; 
 				break;
 			case "yellow": 
-				color = Color.yellow; 
+				color = new Color(1, 1, 0, 1);
 				break;
 			case "green": 
 				color = Color.green; 
