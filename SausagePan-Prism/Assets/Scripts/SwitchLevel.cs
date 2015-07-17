fef84fbe5184ab0cc4b502cc432cb3d18d14e7b0
Georg Eckert <eckert.georg@gmx.de>
2015-07-17 17:47:31 +0200

    add credits music

--- SausagePan-Prism/Assets/Scripts/SwitchLevel.cs
+++ SausagePan-Prism/Assets/Scripts/SwitchLevel.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class SwitchLevel : MonoBehaviour {
 
 	public void goToLevel(string level) {
-		if(level.Equals("Startbildschirm") && Application.loadedLevel != 16)
+		if(level.Equals("Startbildschirm") && Application.loadedLevel != 16 && Application.loadedLevel != 25)
 		{	
 			var go = GameObject.Find ("audio4");
 			AudioSource help = go.GetComponent<AudioSource> ();
